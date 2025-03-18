88f79aac18b23f962a150f761979e813ab16ea7a
Joaoconte5 <Joaovitor_conte@hotmail.com>
2025-03-18 19:44:46 -0300

    Add files via upload

--- Aplicacao.py
+++ Aplicacao.py
@@ -9,27 +9,23 @@ from io import StringIO
 GDRIVE_CSV_URL = "https://drive.google.com/uc?id=12zWDtYgLGE0-u_aXKmPFQJbYj5WTuq-k"
 
 def autenticar_usuario():
-    # Verifica se o usuário já está autenticado
     if "autenticado" in st.session_state and st.session_state["autenticado"]:
-        return  # Se já estiver autenticado, não faz nada
-    
+        return
     st.sidebar.title("Login")
     usuario = st.sidebar.text_input("Usuário")
     senha = st.sidebar.text_input("Senha", type="password")
-    
     if st.sidebar.button("Entrar"):
-        # Condição de login para os dois usuários
         if (usuario == "Suellen" and senha == "1234") or (usuario == "João" and senha == "5678"):
             st.session_state["autenticado"] = True
             st.session_state["usuario"] = usuario
-            st.experimental_rerun()  # Rerun para reiniciar a aplicação com o estado atualizado
+            st.experimental_rerun()
         else:
             st.sidebar.error("Usuário ou senha incorretos")
 
 def carregar_despesas():
     try:
         response = requests.get(GDRIVE_CSV_URL)
-        response.raise_for_status()  # Verifica se houve erro de conexão
+        response.raise_for_status()
         csv_data = StringIO(response.text)
         return pd.read_csv(csv_data)
     except requests.exceptions.RequestException as e:
@@ -42,7 +38,6 @@ def salvar_despesa(data, data_vencimento, categoria, origem, valor, cartao, parc
     try:
         historico = carregar_despesas()
         historico = pd.concat([historico, df], ignore_index=True)
-        # Placeholder para salvar despesas no Google Drive:
         st.warning("Para salvar no Google Drive, configure a API do Google Drive.")
     except Exception as e:
         st.error(f"Erro ao salvar despesa: {e}")
@@ -51,7 +46,6 @@ def deletar_despesa(index):
     despesas = carregar_despesas()
     if 0 <= index < len(despesas):
         despesas = despesas.drop(index).reset_index(drop=True)
-        # Placeholder para salvar despesas após deletar:
         st.warning("Para salvar no Google Drive, configure a API do Google Drive.")
 
 def editar_despesa(index, data, data_vencimento, categoria, origem, valor, cartao, parcelas, observacao):
@@ -65,39 +59,53 @@ def editar_despesa(index, data, data_vencimento, categoria, origem, valor, carta
         despesas.at[index, "Cartão"] = cartao
         despesas.at[index, "Parcelas"] = parcelas
         despesas.at[index, "Observação"] = observacao
-        # Placeholder para salvar despesas após editar:
         st.warning("Para salvar no Google Drive, configure a API do Google Drive.")
 
 def pagina_lancamento():
     st.title("Lançamento de Despesas")
     
     despesas = carregar_despesas()
-    despesa_editar = None
-    index_editar = st.number_input("Informe o índice da despesa a ser editada", min_value=0, max_value=len(despesas)-1 if not despesas.empty else 0, step=1)
-    
-    if not despesas.empty:
-        despesa_editar = despesas.iloc[index_editar]
-    
-    data = st.date_input("Data da Despesa", format="DD/MM/YYYY", value=date.today() if despesa_editar is None else despesa_editar["Data"])
-    data_vencimento = st.date_input("Data de Vencimento", format="DD/MM/YYYY", value=date.today() if despesa_editar is None else despesa_editar["Data de Vencimento"])
-    categoria = st.selectbox("Categoria", ["Mercado", "Delivery Comida", "Delivery Bebida", "Restaurante/Bar", "Saúde", "Lazer", "Viagem", "Casa Utensílio", "Casa Concerto", "Diversos", "Carro",
-                                          "Esportes", "Uber/99", "Entretenimento", "Nina", "Beleza", "Vestuário", "Cursos e Treinamentos"],
-                            index=["Mercado", "Delivery Comida", "Delivery Bebida", "Restaurante/Bar", "Saúde", "Lazer", "Viagem", "Casa Utensílio", "Casa Concerto", "Diversos", "Carro",
-                                   "Esportes", "Uber/99", "Entretenimento", "Nina", "Beleza", "Vestuário", "Cursos e Treinamentos"].index(despesa_editar["Categoria"]) if despesa_editar is not None else 0)
-    origem = st.selectbox("Origem", ["Suellen", "João", "Conjunto"], index=["Suellen", "João", "Conjunto"].index(despesa_editar["Origem"]) if despesa_editar is not None else 0)
-    valor = st.number_input("Valor", min_value=0.0, format="%.2f", value=despesa_editar["Valor"] if despesa_editar is not None else 0.0)
-    cartao = st.selectbox("Cartão", ["Crédito", "Débito"], index=["Crédito", "Débito"].index(despesa_editar["Cartão"]) if despesa_editar is not None else 0)
-    parcelas = st.number_input("Parcelas", min_value=1, step=1, value=int(despesa_editar["Parcelas"]) if despesa_editar is not None else 1, key="input_parcelas_edit")
-    observacao = st.text_area("Observação", value=despesa_editar["Observação"] if despesa_editar is not None else "")
-    
-    if st.button("Salvar Despesa"):
-        if despesa_editar is None:  # Novo lançamento
+    opcao_edicao = st.radio("Você quer editar uma despesa existente ou adicionar uma nova?", ("Adicionar Nova Despesa", "Editar Despesa Existente"))
+
+    if opcao_edicao == "Adicionar Nova Despesa":
+        # Campos para adicionar uma nova despesa
+        data = st.date_input("Data da Despesa", format="DD/MM/YYYY", value=date.today())
+        data_vencimento = st.date_input("Data de Vencimento", format="DD/MM/YYYY", value=date.today())
+        categoria = st.selectbox("Categoria", ["Mercado", "Delivery Comida", "Delivery Bebida", "Restaurante/Bar", "Saúde", "Lazer", "Viagem", "Casa Utensílio", "Casa Concerto", "Diversos", "Carro",
+                                              "Esportes", "Uber/99", "Entretenimento", "Nina", "Beleza", "Vestuário", "Cursos e Treinamentos"])
+        origem = st.selectbox("Origem", ["Suellen", "João", "Conjunto"])
+        valor = st.number_input("Valor", min_value=0.0, format="%.2f")
+        cartao = st.selectbox("Cartão", ["Crédito", "Débito"])
+        parcelas = st.number_input("Parcelas", min_value=1, step=1, value=1)
+        observacao = st.text_area("Observação")
+        
+        if st.button("Salvar Despesa"):
             salvar_despesa(data, data_vencimento, categoria, origem, valor, cartao, parcelas, observacao)
             st.success("Despesa salva com sucesso!")
-        else:  # Editando lançamento existente
-            editar_despesa(index_editar, data, data_vencimento, categoria, origem, valor, cartao, parcelas, observacao)
-            st.success("Despesa editada com sucesso!")
-        st.rerun()
+            st.rerun()
+
+    elif opcao_edicao == "Editar Despesa Existente":
+        # Campos para editar uma despesa existente
+        index_editar = st.number_input("Informe o índice da despesa a ser editada", min_value=0, max_value=len(despesas)-1 if not despesas.empty else 0, step=1)
+        
+        if len(despesas) > 0:
+            despesa_editar = despesas.iloc[index_editar]
+            data = st.date_input("Data da Despesa", format="DD/MM/YYYY", value=despesa_editar["Data"])
+            data_vencimento = st.date_input("Data de Vencimento", format="DD/MM/YYYY", value=despesa_editar["Data de Vencimento"])
+            categoria = st.selectbox("Categoria", ["Mercado", "Delivery Comida", "Delivery Bebida", "Restaurante/Bar", "Saúde", "Lazer", "Viagem", "Casa Utensílio", "Casa Concerto", "Diversos", "Carro",
+                                                  "Esportes", "Uber/99", "Entretenimento", "Nina", "Beleza", "Vestuário", "Cursos e Treinamentos"],
+                                     index=["Mercado", "Delivery Comida", "Delivery Bebida", "Restaurante/Bar", "Saúde", "Lazer", "Viagem", "Casa Utensílio", "Casa Concerto", "Diversos", "Carro",
+                                            "Esportes", "Uber/99", "Entretenimento", "Nina", "Beleza", "Vestuário", "Cursos e Treinamentos"].index(despesa_editar["Categoria"]))
+            origem = st.selectbox("Origem", ["Suellen", "João", "Conjunto"], index=["Suellen", "João", "Conjunto"].index(despesa_editar["Origem"]))
+            valor = st.number_input("Valor", min_value=0.0, format="%.2f", value=despesa_editar["Valor"])
+            cartao = st.selectbox("Cartão", ["Crédito", "Débito"], index=["Crédito", "Débito"].index(despesa_editar["Cartão"]))
+            parcelas = st.number_input("Parcelas", min_value=1, step=1, value=despesa_editar["Parcelas"])
+            observacao = st.text_area("Observação", value=despesa_editar["Observação"])
+            
+            if st.button("Salvar Despesa Editada"):
+                editar_despesa(index_editar, data, data_vencimento, categoria, origem, valor, cartao, parcelas, observacao)
+                st.success("Despesa editada com sucesso!")
+                st.rerun()
 
     st.subheader("Histórico de Despesas")
     st.dataframe(despesas)
